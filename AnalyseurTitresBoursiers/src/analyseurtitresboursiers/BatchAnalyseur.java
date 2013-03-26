8caad0c92f3f469e7a57c1d27c8ff0ff1edff288
jocelynm_ap203@hotmail.com <jocelynm_ap203@hotmail.com>
2013-03-25 20:00:01 -0400

    Ajout d'une liste des titres suivis dans le courriel

--- AnalyseurTitresBoursiers/src/analyseurtitresboursiers/BatchAnalyseur.java
+++ AnalyseurTitresBoursiers/src/analyseurtitresboursiers/BatchAnalyseur.java
@@ -15,85 +15,103 @@ import java.util.*;
  * @author jocelynm
  */
 public class BatchAnalyseur {
-    
-    private final static String MSG_HEADER = "Bonjour, \nVoici votre rapport quotidien\n\n";
+
+    private final static String MSG_HEADER = "Bonjour, \n\nVoici votre rapport quotidien\n\n";
     private final static String MSG_ACHAT = "Recommandations ACHAT:\n";
     private final static String MSG_VENTE = "Recommandations VENTE:\n";
     private final static String MSG_SAUT_SECTION = "\n\n";
+    private final static String MSG_ACTION_REQUISE = "Aucune action suggérée";
+    private final static String MSG_ACTIONS_SUIVIS = "Liste des actions suivis:\n";
     private final static String MSG_FOOTER = "\u00a9 Forest/Meilleur - 2013";
-    
     private List<TitreBoursier> recommendationsAchat = new ArrayList();
     private List<TitreBoursier> recommendationsVente = new ArrayList();
-    
+    private List<TitreBoursier> actionsSuivis = new ArrayList();
     private DatabaseLayor databaseLayor;
-    
 
-    public BatchAnalyseur(DatabaseLayor databaseLayor) {    
+    public BatchAnalyseur(DatabaseLayor databaseLayor) {
         this.databaseLayor = databaseLayor;
     }
-    
+
     public void traiter() throws IOException, MalformedURLException, ParseException, SQLException {
-        
+
         GregorianCalendar gc = new GregorianCalendar();
         gc.add(Calendar.MONTH, -2);
         Date debut = gc.getTime();
-        
+
         for (TitreBoursier titreAnalyse : this.databaseLayor.obtenirTitreEnLot()) {
-            
+
             ArrayList<TitreBoursier> historique = this.databaseLayor.obtenirHistorique(titreAnalyse.getTitre(), debut);
             AnalysteMacd analyste = new AnalysteMacd(historique);
-            //System.out.println("Taille de l'historique: " + historique.size());
-            //System.out.println("Taille de l'analyste: " + analyste.getCotesBoursieres().size());
+
             TitreBoursier titreBoursier = historique.get(historique.size() - 1);
+
+            actionsSuivis.add(titreBoursier);
 
             if (analyste.estAchatBatch()) {
                 recommendationsAchat.add(titreBoursier);
-            } 
+            }
 
             if (analyste.estVenteBatch()) {
                 recommendationsVente.add(titreBoursier);
-            } 
+            }
         }
     }
-    
+
     public List<TitreBoursier> getRecommandationsAchat() {
         return recommendationsAchat;
     }
-    
+
     public List<TitreBoursier> getRecommandationsVente() {
         return recommendationsVente;
     }
-    
+
+    public List<TitreBoursier> getListeActionSuivis() {
+        return actionsSuivis;
+    }
+
     public void envoyerCourriel(String titre) {
-        
+
         String msgBody = "";
-        
-//        System.out.println("Taille des recommandations ACHAT: " + this.getRecommandationsAchat().size());
-//        System.out.println("Taille des recommandations VENTE: " + this.getRecommandationsVente().size());
+
         msgBody = msgBody + MSG_HEADER;
-        
+
         msgBody = msgBody + MSG_ACHAT;
-        
-        for (TitreBoursier titreBoursier : this.getRecommandationsAchat()) {
-            String msg = String.format("%s (%s): %.2f$ | %s", titreBoursier.getTitre(), titreBoursier.getDescription(), titreBoursier.getValeurFermeture(), titreBoursier.getDateFermeture()); 
-            msgBody = msgBody +  msg;
+
+        if (0 < this.getRecommandationsAchat().size()) {
+            for (TitreBoursier titreBoursier : this.getRecommandationsAchat()) {
+                String msg = String.format("%s (%s): %.2f$ | %s", titreBoursier.getTitre(), titreBoursier.getDescription(), titreBoursier.getValeurFermeture(), titreBoursier.getDateFermeture());
+                msgBody = msgBody + msg;
+            }
+        } else {
+            msgBody = msgBody + MSG_ACTION_REQUISE;
         }
-        
+
         msgBody = msgBody + MSG_SAUT_SECTION;
-        
+
         msgBody = msgBody + MSG_VENTE;
-        
-        for (TitreBoursier titreBoursier : this.getRecommandationsVente()) {
-            String msg = String.format("%s (%s): %.2f$ | %s", titreBoursier.getTitre(), titreBoursier.getDescription(), titreBoursier.getValeurFermeture(), titreBoursier.getDateFermeture()); 
-            msgBody = msgBody +  msg;
+
+        if (0 < this.getRecommandationsVente().size()) {
+            for (TitreBoursier titreBoursier : this.getRecommandationsVente()) {
+                String msg = String.format("%s (%s): %.2f$ | %s", titreBoursier.getTitre(), titreBoursier.getDescription(), titreBoursier.getValeurFermeture(), titreBoursier.getDateFermeture());
+                msgBody = msgBody + msg;
+            }
+        } else {
+            msgBody = msgBody + MSG_ACTION_REQUISE;
         }
-        
+
         msgBody = msgBody + MSG_SAUT_SECTION;
-        
+
+        msgBody = msgBody + MSG_ACTIONS_SUIVIS;
+
+        for (TitreBoursier titreBoursier : this.getListeActionSuivis()) {
+            String msg = String.format("%s (%s)\n", titreBoursier.getTitre(), titreBoursier.getDescription());
+            msgBody = msgBody + msg;
+        }
+
+        msgBody = msgBody + MSG_SAUT_SECTION;
         msgBody = msgBody + MSG_FOOTER;
 
         MailLayor.send(titre, msgBody);
         //System.out.println(titre + "\n" + msgBody);
     }
-    
 }
